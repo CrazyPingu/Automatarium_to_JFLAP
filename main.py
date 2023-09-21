8ddc578bbb7484ffab2b0d40e15b842e5460ebdd
Samuele De Tuglie <samuele.de.tuglie@gmail.com>
2023-09-21 22:21:51 +0200

    Fix coordinates

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import os
 import json
 import xml.etree.ElementTree as ET
 
-# Function to convert JSON to XML
+
+# Function to convert JSON to JFF/XML and normalize coordinates
 def convert_json_to_xml(input_file, output_file):
     with open(input_file, "r") as json_file:
         json_data = json.load(json_file)
+
+    # Find the minimum x and y coordinates
+    min_x = min(state["x"] for state in json_data["states"]) - 50 # Subtract 50 to add some padding
+    min_y = min(state["y"] for state in json_data["states"])
 
     # Create the XML root element
     root = ET.Element("structure")
@@ -19,15 +24,15 @@ def convert_json_to_xml(input_file, output_file):
     # Create and append the <automaton> element
     automaton_element = ET.SubElement(root, "automaton")
 
-    # Add states to the automaton
+    # Add states to the automaton with normalized coordinates
     for state in json_data["states"]:
         state_element = ET.SubElement(automaton_element, "state")
         state_element.attrib["id"] = str(state["id"])
         state_element.attrib["name"] = "q" + str(state["id"])
         x_element = ET.SubElement(state_element, "x")
-        x_element.text = str(state["x"])
+        x_element.text = str(state["x"] - min_x)  # Normalize x coordinate
         y_element = ET.SubElement(state_element, "y")
-        y_element.text = str(state["y"])
+        y_element.text = str(state["y"] - min_y)  # Normalize y coordinate
         if "label" in state:
             label_element = ET.SubElement(state_element, "label")
             label_element.text = state["label"]
@@ -44,8 +49,6 @@ def convert_json_to_xml(input_file, output_file):
 
     # Create the XML tree
     tree = ET.ElementTree(root)
-
-    # Write the XML to a file or print it
     tree.write(output_file, encoding="utf-8", xml_declaration=True)
 
 if __name__ == "__main__":
@@ -53,7 +56,7 @@ if __name__ == "__main__":
     input_folder = "Automatarium_file"
 
     # Define the output folder where XML files will be saved
-    output_folder = "output"
+    output_folder = "Output"
 
     # Ensure the output folder exists
     if not os.path.exists(output_folder):
@@ -66,9 +69,5 @@ if __name__ == "__main__":
             xml_file_name = os.path.splitext(filename)[0] + ".jff"
             xml_file_path = os.path.join(output_folder, xml_file_name)
 
-            # Convert JSON to XML and save it in the output folder
+            # Convert JSON to JFF/XML with normalized coordinates and save it in the output folder
             convert_json_to_xml(json_file_path, xml_file_path)
-
-
-
-
